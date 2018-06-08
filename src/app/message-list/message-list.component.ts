dea2e7d67728a2c34819a4c921cbae4e42f6f8ef
fcaceres <fcaceres@portalfinance.co>
2018-06-08 15:47:38 -0500

    scroll message component

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,19 +1,53 @@
-import { MessageVM } from './../message-section/message.vm';
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ElementRef, ViewChild, OnChanges, SimpleChanges} from '@angular/core';
+import {MessageVM} from "../message-section/message.vm";
+import * as _ from 'lodash';
 
 @Component({
-  selector: 'message-list',
-  templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+    selector: 'message-list',
+    templateUrl: './message-list.component.html',
+    styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnChanges {
 
-  @Input()
-  messages:MessageVM[];
+    @Input()
+    messages: MessageVM[] = [];
 
-  constructor() { }
+    @ViewChild('list')
+    list: ElementRef;
 
-  ngOnInit() {
-  }
+
+    constructor() {
+
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+
+        if (changes['messages']) {
+
+            const previousMessages = changes['messages'].previousValue;
+
+            const newMessages = changes['messages'].currentValue;
+
+            if (previousMessages && newMessages.length > previousMessages.length) {
+                setTimeout(() => {
+                    this.scrollLastMessageIntoView();
+                });
+            }
+
+
+        }
+
+    }
+
+    scrollLastMessageIntoView() {
+        const items = this.list.nativeElement.querySelectorAll('li');
+        const lastItem: any = _.last(items);
+        if (lastItem) {
+            lastItem.scrollIntoView();
+        }
+    }
+
+
+
 
 }
